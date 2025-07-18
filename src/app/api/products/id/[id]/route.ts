69f42519eec83f105e45971a9f36a0743148cb58
ganidugunasekara91@gmail.com <ganidugunasekara91@gmail.com>
2025-07-18 18:10:27 +0530

    change the get mmethod

--- src/app/api/products/id/[id]/route.ts
+++ src/app/api/products/id/[id]/route.ts
@@ -2,13 +2,20 @@ import { NextRequest } from "next/server";
 
 const BACKEND_URL = process.env.NEXT_PUBLIC_NEST_API_URL;
 
-export async function GET(req: NextRequest, {params} : {params : {id : string}}) {
+type Params = {
+  params: {
+    id: string;
+  };
+};
 
-    const res = await fetch (`${BACKEND_URL}/products/id/${params.id}`, {cache : "no-store"})
-    const data = await res.json();
+export async function GET(req: NextRequest, { params }: Params) {
+  const res = await fetch(`${BACKEND_URL}/products/id/${params.id}`, {
+    cache: "no-store",
+  });
+  const data = await res.json();
 
-    return new Response(JSON.stringify(data),{
-        status : res.status,
-        headers: { "Content-Type": "application/json" },
-    })
+  return new Response(JSON.stringify(data), {
+    status: res.status,
+    headers: { "Content-Type": "application/json" },
+  });
 }
